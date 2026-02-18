e1bbb02ffd31e275c495879b4598ad08315fd9d6
joel rivera <107011173+joelrivera14@users.noreply.github.com>
2026-02-18 13:33:00 -0500

    Time: 0 ms (100.00%) | Memory: 8.4 MB (38.22%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     char repeatedCharacter(string s) {
-       unordered_map<char, int> map;
-       for(int i = 0; i<s.size(); ++i){
-        char c = s[i];
-        if (map.contains(c)){
-            return c;
+       unordered_set<char>setHolder;
+       for(char c : s){
+        if(setHolder.contains(c)){
+            return {c};
         }
-        map[c]= i;
-       }
-       return 0; 
+        setHolder.insert(c);
+       } 
+       return 0;
     }
 };
