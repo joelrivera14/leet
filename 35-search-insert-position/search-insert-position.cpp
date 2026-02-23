e0041d9d02a644e850e5b78bb2dd27ec57e26703
joel rivera <107011173+joelrivera14@users.noreply.github.com>
2026-02-23 17:03:07 -0500

    Time: 0 ms (100.00%) | Memory: 13.6 MB (76.36%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int pivot = 0, left = 0, right =nums.size()-1;
+        int pivot = 0, left = 0, right = nums.size()-1;
         while(left<=right){
-            pivot = left + (right-left)/2;
-            if(nums[pivot] == target){
+            pivot = left+ (right-left) /2;
+            if(target == nums[pivot]){
                 return pivot;
             }
             else if(nums[pivot] > target){
                 right = pivot -1;
             }
             else{
-                
                 left = pivot +1;
             }
         }
