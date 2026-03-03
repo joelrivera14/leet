e0073ad5651a84552b737e5afd6c9f88bcacd37c
joel rivera <107011173+joelrivera14@users.noreply.github.com>
2026-03-03 11:18:30 -0500

    Time: 1 ms (53.73%) | Memory: 12.5 MB (79.48%) - LeetSync

---
+++ 1098-largest-unique-number/largest-unique-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int largestUniqueNumber(vector<int>& nums) {
+        unordered_map<int, int> count;
+        for(int n : nums){
+            count[n]++;
+        }
+        int largestUniqueNum = -1;
+
+        for(auto[key,value] : count){
+            if(value == 1){
+                if (key>largestUniqueNum){
+                    largestUniqueNum = key;
+                }
+            }
+        }
+        return largestUniqueNum;
+
+    }
+};
