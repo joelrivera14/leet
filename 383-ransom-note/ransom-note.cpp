85146650f98d562e067178c962ad27b7d02b5641
joel rivera <107011173+joelrivera14@users.noreply.github.com>
2026-03-09 14:05:21 -0400

    Time: 11 ms (10.18%) | Memory: 11.9 MB (49.43%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+      unordered_map<char, int> rCount;
+      for(char& c : ransomNote){
+        rCount[c]++;
+      } 
+
+      for(char& m : magazine){
+        if(rCount.find(m) != rCount.end()){
+            rCount[m]--;
+        }
+      }
+      for(auto&[key, value] : rCount){
+        if(value>0){
+            return false;
+        }
+      }
+      return true;
+    }
+};
