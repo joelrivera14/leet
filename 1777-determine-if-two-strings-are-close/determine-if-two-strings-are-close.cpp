8b19f23f50a08b53fb8c8c7ed474306baef8759f
joel rivera <107011173+joelrivera14@users.noreply.github.com>
2026-03-20 20:45:17 -0400

    Time: 16 ms (48.59%) | Memory: 23.4 MB (84.24%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -5,20 +5,20 @@ public:
         vector<int> string2(26,0);
 
         for(char& c : word1){
-            string1[c -'a']++;
+            string1[c-'a']++;
         }
         for(char& c : word2){
-            string2[c -'a']++;
+            string2[c-'a']++;
         }
 
-        for(int i = 0; i<26; ++i){
+        for(int i = 0; i<string1.size(); ++i){
             if(string1[i] == 0 && string2[i] != 0 || string2[i] == 0 && string1[i] != 0){
                 return false;
             }
         }
+
         sort(string1.begin(), string1.end());
         sort(string2.begin(), string2.end());
-
         for(int i = 0; i<string1.size(); ++i){
             if(string1[i]!=string2[i]){
                 return false;
