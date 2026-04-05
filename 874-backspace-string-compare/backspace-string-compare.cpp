cd69d25f155fd12e57a3f19ffaebbcd15edf1ca4
joel rivera <107011173+joelrivera14@users.noreply.github.com>
2026-04-05 17:54:06 -0400

    Time: 0 ms (100.00%) | Memory: 8.4 MB (86.13%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        string newS = "";
+        string newT = "";
+
+        for(char c : s){
+            if( c == '#'){
+                if(!newS.empty()){
+                newS.pop_back();
+
+                }
+            }else{
+                newS.push_back(c);
+            }
+        }
+        for(char c : t){
+            if( c == '#'){
+                if(!newT.empty()){
+
+                newT.pop_back();
+                }
+            }else{
+                newT.push_back(c);
+            }
+        }
+        return newS == newT;
+    }
+};
